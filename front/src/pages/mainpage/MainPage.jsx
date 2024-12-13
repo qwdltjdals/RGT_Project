df4fada3ce863464d4f59638cd944c99c9b55225
Skeat <iu_love@kakao.com>
2024-12-13 12:22:11 +0900

    검색 기능 추가중

--- front/src/pages/mainpage/MainPage.jsx
+++ front/src/pages/mainpage/MainPage.jsx
@@ -1,88 +1,116 @@
 /** @jsxImportSource @emotion/react */
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import * as s from "./style";
 import { instance } from "../../apis/util/instance";
 import { useState } from "react";
+import Search from "../../components/search/Search";
 
-function MainPage(props) {
+function MainPage() {
+    const [page, setPage] = useState(1);
+    const [limit, setLimit] = useState(10);
+    const [searchType, setSearchType] = useState("title");
+    const [isSearching, setIsSearching] = useState(false);
 
-        // 페이지와 페이지 당 항목 수 상태 관리
-        const [page, setPage] = useState(1);
-        const [limit, setLimit] = useState(10); // 한 페이지당 10개 항목
-
-    const { data, isError, isLoading, refetch } = useQuery(
+    const { data: allBooks, isError, isLoading: isLoadingAll } = useQuery(
         ["getBookList", page, limit],
         async () => {
             return await instance.get("api/books", {
-                params : {page, limit}
+                params: { page, limit }
             });
         },
         {
-            onSuccess : (response) => {
-                console.log(response)
-            },
-            retry : 0,
+            enabled: !isSearching,
+            retry: 0,
             refetchOnWindowFocus: false
         }
     );
 
-    // 페이지 변경 함수
+    const searchBooks = useMutation(
+        async ({ page, limit, searchDto }) => {
+            const dto = {title: searchDto.title, author: searchDto.author, page, limit}
+            console.log(dto)
+            return await instance.post("api/books/search", dto);
+        }
+    );
+
+    const handleSearch = (searchDto) => {
+        setIsSearching(true);
+        searchBooks.mutate(
+            { searchDto, page, limit },
+            {
+                onSuccess: () => {
+                    setIsSearching(false);
+                }
+            }
+        );
+    };
+
     const handlePageChange = (newPage) => {
         setPage(newPage);
     };
 
+    if (isLoadingAll) {
+        return <div>데이터를 불러오는 중입니다...</div>;
+    }
+
+    if (isError) {
+        return <div>데이터를 불러오는데 실패했습니다. 다시 시도해주세요.</div>;
+    }
+
     return (
-        <>
-            <div css={s.layout}>
-                <div css={s.searchBox}>
-                    검색 기능 추가
-                </div>
-                <div css={s.contentBox}>
-                    <table>
-                        <thead>
-                            <tr>
-                                <td>id</td>
-                                <td>제목</td>
-                                <td>이미지</td>
-                                <td>저자</td>
-                                <td>출판사</td>
-                                <td>금액</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {data?.data?.books.map((book) => (
+        <div css={s.layout}>
+            <div css={s.contentBox}>
+                <Search onSearch={handleSearch}/>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>제목</th>
+                            <th>이미지</th>
+                            <th>저자</th>
+                            <th>출판사</th>
+                            <th>금액</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {allBooks?.data?.books.map((book) => (
                             <tr key={book.id}>
                                 <td>{book.id}</td>
                                 <td>{book.title}</td>
-                                <td><img src={book.image} alt={book.title} /></td>
+                                <td>
+                                    <img
+                                        src={book.img}
+                                        alt={book.title}
+                                        style={{ maxWidth: "100px", height: "auto" }}
+                                    />
+                                </td>
                                 <td>{book.author}</td>
                                 <td>{book.publisher}</td>
                                 <td>{book.price}</td>
                             </tr>
                         ))}
-                        </tbody>
-                    </table>
-                            <div>
-                                <button 
-                                    onClick={() => handlePageChange(page - 1)} 
-                                    disabled={page === 1}
-                                >
-                                이전
-                                </button>
-                                <span>{page}</span>
-                                <button 
-                                    onClick={() => handlePageChange(page + 1)} 
-                                    disabled={data && data.data.length < limit}
-                                >
-                                    다음
-                                </button>
-                            </div>
-                        <div css={s.buttonBox}>
-                                <button>도서 추가</button>
-                            </div>
-                    </div>
+                    </tbody>
+                </table>
+                <div>
+                    <button 
+                        onClick={() => handlePageChange(page - 1)} 
+                        disabled={page === 1}
+                    >
+                        이전
+                    </button>
+                    <span>{page}</span>
+                    <button 
+                        onClick={() => handlePageChange(page + 1)} 
+                        disabled={allBooks && allBooks.data.books.length < limit}
+                    >
+                        다음
+                    </button>
+                </div>
+                <div css={s.buttonBox}>
+                    <button>도서 추가</button>
+                </div>
             </div>
-        </>
+        </div>
     );
 }
 
